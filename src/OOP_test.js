1d9dab0c4752927daf35611e476563b4c0e01284
Josh Murr <joshuadavidmurr@gmail.com>
2020-10-26 16:11:44 +0000

    Remove unused files

--- src/OOP_test.js
+++
@@ -1,6 +0,0 @@
-import Model from './model_class.js';
-import Downsample from './downsample_class.js';
-
-const gl = document.getElementById('c').getContext('webgl2');
-
-const model = new Model(gl, {});
--- src/example_conv.js
+++
@@ -1,9 +0,0 @@
-import Downsample from './downscale_class.js';
-
-const D1 = new Downsample(gl, {
-  vs_source: require('./glsl/basic_vert.glsl'),
-  fs_source: require('./glsl/downscale_frag.glsl'),
-  filter_size: 4,
-  num_filters: 4,
-  output_size: 16,
-});
